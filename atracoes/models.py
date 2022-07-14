fbb187a29c080814a19eed9e2841d16a1c480771
BrunoAlz <bruno6821@gmail.com>
2022-07-14 14:04:08 -0300

    Criação dos Models Ponto Turistico e Atrações

--- atracoes/models.py
+++ atracoes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Atracao(models.Model):
+
+    nome = models.CharField(max_length=250)
+    descricao = models.TextField()
+    horario_funcionamento = models.CharField(max_length=250)
+    idade_minima = models.PositiveIntegerField()
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+
+    class Meta:
+        verbose_name = ("Atração")
+        verbose_name_plural = ("Atrações")
+
+    def __str__(self):
+        return self.nome
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from atracoes.models import Atracao
 
-# Create your models here.
+
+class PontoTuristico(models.Model):
+    
+    nome = models.CharField(max_length=250)
+    descricao = models.TextField()
+    aprovado = models.BooleanField(default=False)
+    atracoes = models.ManyToManyField(Atracao)
+
+    class Meta:
+        verbose_name = ("Ponto Turistico")
+        verbose_name_plural = ("Pontos Turisticos")
+
+    def __str__(self):
+        return self.nome
+
